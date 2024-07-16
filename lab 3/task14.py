bfbd69d2c2635244f0f9b9498c32cd0b37b4a928
Mirza-Moiz <moiznadeem1243@gmail.com>
2024-07-16 23:40:54 +0500

    Added: Lab 3 - Tasks

--- lab 3/task14.py
+++ lab 3/task14.py
@@ -1,28 +1,16 @@
 def main():
-    password = input("Enter a password: ")
+    string = input("Enter a string: ")
     
-    if len(password) < 6 or len(password) > 16:
-        print("Password must be between 6 and 16 characters")
-        return
+    letters = 0
+    digits = 0
     
-    has_lower = False
-    has_upper = False
-    has_digit = False
-    has_special = False
-    
-    for char in password:
-        if char.islower():
-            has_lower = True
-        elif char.isupper():
-            has_upper = True
+    for char in string:
+        if char.isalpha():
+            letters += 1
         elif char.isdigit():
-            has_digit = True
-        elif char in "$#@":
-            has_special = True
+            digits += 1
     
-    if has_lower and has_upper and has_digit and has_special:
-        print("Valid password")
-    else:
-        print("Invalid password")
-        
+    print(f"Letters {letters}")
+    print(f"Digits {digits}")
+    
 main()
